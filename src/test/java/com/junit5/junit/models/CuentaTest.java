1a93a5eae8718425d16d018b431b55018c23ce13
Jonathan Ramirez <68352096+jonathanramirezislas@users.noreply.github.com>
2021-03-26 15:47:16 -0600

    RepeatedTest

--- src/test/java/com/junit5/junit/models/CuentaTest.java
+++ src/test/java/com/junit5/junit/models/CuentaTest.java
@@ -280,6 +280,20 @@ class CuentaTest {
         assertTrue(cuenta.getSaldo().compareTo(BigDecimal.ZERO) > 0);
     }
 
+    //repetir un test un numero determinado de veces  y displayname nos ayuda a nombre del test
+    @DisplayName("Probando Debito Cuenta Repetir!")
+    @RepeatedTest(value=5, name = "{displayName} - Repetición numero {currentRepetition} de {totalRepetitions}")
+    void testDebitoCuentaRepetir(RepetitionInfo info) {
+        if(info.getCurrentRepetition() == 3){
+            System.out.println("estamos en la repeticion " + info.getCurrentRepetition());
+        }
+        cuenta.debito(new BigDecimal(100));
+        assertNotNull(cuenta.getSaldo());
+        assertEquals(900, cuenta.getSaldo().intValue());
+        assertEquals("900.12345", cuenta.getSaldo().toPlainString());
+    }
+
+
 }
 
 
